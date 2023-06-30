f48c35a3804fa4ab306c6f50c6903eff2c3748ce
Curtis Myzie <curtis.myzie@gmail.com>
2023-06-30 17:07:22 -0400

    Wildcard route?

---
+++ pages/api/[rsc].ts
@@ -0,0 +1,20 @@
+import type { NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async function handler(req: NextRequest) {
+  return new Response(
+    JSON.stringify({
+      name: process.env.NEXT_RUNTIME,
+      ugh: 'RESOURCE',
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+  );
+}
